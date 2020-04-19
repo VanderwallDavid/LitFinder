9d875478d8c6b14a0533245efe86e7036f073cc3
VanderwallDavid <63895455+VanderwallDavid@users.noreply.github.com>
2020-04-18 22:18:44 -0500

    Add files via upload

---
+++ PRacticePArse.py
@@ -0,0 +1,144 @@
+resulttext="""
+1. Elife. 2020 Apr 16;9. pii: e57519. doi: 10.7554/eLife.57519. [Epub ahead of
+print]
+
+The histone deacetylase complex MiDAC regulates a neurodevelopmental gene
+expression program to control neurite outgrowth.
+
+Mondal B(1), Jin H(1), Kallappagoudar S(2), Sedkov Y(1), Martinez T(1), Sentmanat
+MF(3), Poet GJ(4), Li C(4), Fan Y(5), Pruett-Miller SM(6), Herz HM(1).
+
+Author information: 
+(1)Cell & Molecular Biology, St Jude Children's Research Hospital, Memphis,
+United States.
+(2)Pathology, St Jude Children's Research Hospital, Memphis, United States.
+(3)Genetics, Washington University in St Louis, St Louis, United States.
+(4)Tumor Cell Biology, St Jude Children's Research Hospital, Memphis, United
+States.
+(5)Computational Biology, St Jude Children's Research Hospital, Memphis, United
+States.
+(6)Department of Cell and Molecular Biology, St Jude Children's Research
+Hospital, Memphis, United States.
+
+The mitotic deacetylase complex (MiDAC) is a recently identified histone
+deacetylase (HDAC) complex. While other HDAC complexes have been implicated in
+neurogenesis, the physiological role of MiDAC remains unknown. Here, we show that
+MiDAC constitutes an important regulator of neural differentiation. We
+demonstrate that MiDAC functions as a modulator of a neurodevelopmental gene
+expression program and binds to important regulators of neurite outgrowth. MiDAC 
+upregulates gene expression of pro-neural genes such as those encoding the
+secreted ligands SLIT3 and NETRIN1 (NTN1) by a mechanism suggestive of H4K20ac
+removal on promoters and enhancers. Conversely, MiDAC inhibits gene expression by
+reducing H3K27ac on promoter-proximal and -distal elements of negative regulators
+of neurogenesis. Furthermore, loss of MiDAC results in neurite outgrowth defects 
+that can be rescued by supplementation with SLIT3 and/or NTN1. These findings
+indicate a crucial role for MiDAC in regulating the ligands of the SLIT3 and NTN1
+signaling axes to ensure the proper integrity of neurite development.
+
+© 2020, Mondal et al.
+
+DOI: 10.7554/eLife.57519 
+PMID: 32297854 
+
+Conflict of interest statement: BM, HJ, SK, YS, TM, MS, GP, CL, YF, SP, HH The
+authors declare that no competing interests exist.
+
+
+2. Int J Clin Exp Pathol. 2020 Feb 1;13(2):295-301. eCollection 2020.
+
+Gene-gene interactions between BMP4 and ARHGAP29 among non-syndromic cleft lip
+only (NSCLO) trios from western Han Chinese population.
+
+Wang Y(1), Shi J(2), Zheng Q(1), Shi B(1), Jia Z(1).
+
+Author information: 
+(1)State Key Laboratory of Oral Diseases & National Clinical Research Center for 
+Oral Diseases, Department of Cleft Lip and Palate, West China Hospital of
+Stomatology, Sichuan University Chengdu, China.
+(2)Division of Growth, Development and Section of Orthodontics, School of
+Dentistry, University of California Los Angeles, USA.
+
+BACKGROUND: Genome-wide association studies (GWAS) have found more than 20 genes 
+associated with a risk of non-syndromic cleft lip with or without cleft palate
+(NSCL/P). However, the interactions between these risk genes have been rarely
+reported.
+METHODS: Here we selected 47 Single Nucleotide Polymorphisms (SNP) from previous 
+GWASs and tested for possible interactions among 302 NSCL/P case-parent trios
+from a western Han Chinese population to further explore the genetic etiology of 
+NSCL/P. Conditional logistic regression models were performed including gene-gene
+(G×G) interaction.
+RESULTS: Twenty pairwise interactions yielded significant p-values. Most of the
+signals of interaction between the SNPs were detected at the same gene including 
+v-maf musculoaponeurotic fibrosarcoma oncogene family, protein B (MAFB), netrin 1
+(NTN1), and single nucleotide polymorphic marker within interferon regulatory
+factor 6 (IRF6). We found evidence of the interaction between rs17563 (bone
+morphogenetic protein 4, BMP4) and rs560426 (subfamily A member 4/Rho GTPase
+activating protein 29, ARHGAP29) (P=0.00093) in NSCLO trios.
+CONCLUSIONS: Gene-gene interaction between markers in BMP4 and ARHGAP29 may
+influence the risk of NSCLO in western Han Chinese population, which might
+explain the missing heritability for NSCL/P.
+
+IJCEP Copyright © 2020.
+
+
+PMCID: PMC7061801
+PMID: 32211112 
+
+Conflict of interest statement: None.
+
+
+3. Health Phys. 2020 Jan 13. doi: 10.1097/HP.0000000000001204. [Epub ahead of print]
+
+Characterization of Primary Human Dermal Fibroblasts to Ensure for Instance EMF
+Exposure Experiments Under Comparable Cell Culture Condition.
+
+Franchini V(1), Müller T, Haupt JM, Ostheim P, Majewski M, Lista F, Port M, Abend
+M(2).
+
+Author information: 
+(1)1Bundeswehr Institute of Radiobiology Munich, Neuherbergstr. 11, 80937 Munich,
+Germany.
+(2)Bundeswehr Institute of Radiobiology Munich, Neuherbergstr. 11, 80937 Munich, 
+Germany.
+
+HDFa (human dermal fibroblasts) are used as cellular models for EMF exposure. To 
+ensure reproducible in vitro experiments, comparable proliferation and
+differentiation cell conditions must exist, and different donors, passage
+numbers, culture time, and growth media must be considered. In this study, the
+authors cultured fibroblasts in DMEM or 106 medium. Growth curves, vitality,
+morphology, and gene expression of genes coding for proliferation (PCNA, CDKN2A, 
+CDKN1A, SFN), differentiation (PDGFRA, TGM2, ACTA2, PDPN, NTN1, MGP, PPP1R14),
+and SFN target genes (TP63, MMP1, MMP3) were examined in both media and passage
+numbers 3-4, 5-6 and >6. At passages 3-4, proliferating cells can be observed in 
+both media. While cells cultured in DMEM proliferate over the passages, from
+passage 5, cells in 106 medium persisted around the seeded number. TGM2
+down-regulation over all passages in both media and cells morphology suggest
+papillary-type fibroblasts. Downregulation of SFN (negative regulator of mitotic 
+translation and cell differentiation) coincided with proliferating fibroblasts
+over all examined conditions. Downstream SFN target genes in proliferating cells 
+appeared upregulated (TP63) and downregulated (MMP1/MMP3), suggestive for a
+status characterized by increased stemnesses (upregulated TP63) and wound healing
+capacity (downregulated MMP1, MMP3). Resting cells (SFN control values) were
+associated with control values of TP63 and MMP1/MMP3 expression, suggesting a
+reduced stemness and wound healing capacity. In conclusion, a set of markers
+related to proliferation (SFN), differentiation (TGM2), stemnesses (TP63), and
+wound healing (MMP1/MMP3) allow a culture characterization so that cells under
+two different conditions can be exposed, thus enabling reproducible EMF
+experiments or experiments with other exposures.
+
+DOI: 10.1097/HP.0000000000001204 
+PMID: 31934933 
+
+
+"""
+
+
+resulttext=resulttext.split()
+print(resulttext)
+for item in resulttext[0:-1]:
+    if item == "DOI:":
+        print(resulttext[resulttext.index("DOI:")+1:resulttext.index("PMID:")])
+        
+        
+
+
